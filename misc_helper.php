302ff41f17e7b7e1582298b50794c3b498d27484
Md. Mamun  Khan Sabuj <mamun.sabuj24@gmail.com>
2021-03-25 18:49:34 +0600

    update helper for create slug

--- misc_helper.php
+++ misc_helper.php
@@ -43,6 +43,92 @@ if(!function_exists('dt')){
 }
 
 
+if ( ! function_exists('remove_space')){
+    function remove_space($str, $limit = false) {
+        if ($limit) {
+            $str = mb_substr($str, 0, $limit, "utf-8");
+        }
+        $text = html_entity_decode($str, ENT_QUOTES, 'UTF-8');
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+        // trim
+        $text = trim($text, '-');
+        return $text;
+    }
+}
+if ( ! function_exists('get_unspace_text'))
+{
+    function get_unspace_text($var = '')    {
+       $string = str_replace(' ','_', $var);
+        return preg_replace('/[^A-Za-z0-9\_]/', '', $string);
+    }
+}
+
+if ( ! function_exists('remove_hyphen'))
+{
+    function remove_hyphen($var = '')    {
+     return  $string = str_replace('-',' ', $var);
+    }
+}
+
+//remove special character
+function clean($string) {
+    return preg_replace('/[^A-Za-z0-9\-\ ]/', '', $string); // Removes special chars.
+}
+
+//This function is used to Generate Key
+    function generator($lenth)
+    {
+        $number=array("A","B","C","D","E","F","G","H","I","J","K","L","N","M","O","P","Q","R","S","U","V","T","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0");
+    
+        for($i=0; $i<$lenth; $i++)
+        {
+            $rand_value=rand(0,34);
+            $rand_number=$number["$rand_value"];
+        
+            if(empty($con))
+            { 
+            $con=$rand_number;
+            }
+            else
+            {
+            $con="$con"."$rand_number";}
+        }
+        return $con;
+    }
+   // Check server alive or not
+if ( ! function_exists('serverAliveOrNot')){
+
+    function serverAliveOrNot($host = '', $port = 80){
+        if(empty($host)){
+            $host = base_url();
+        }
+        $socket = fsockopen($host, $port, $errno, $errstr, 15);
+          if(!$socket)
+          {
+            return false;
+          }
+          else
+          {
+            return true;
+          }
+    }
+}
+
+// check module enabled or not 
+if ( ! function_exists('check_module_status')){
+    function check_module_status($module){
+       $CI =& get_instance();
+       $result = $CI->db->where('directory',$module)->where('status',1)->get('module')->num_rows();
+       if($result>0){
+        return TRUE;
+       }else{
+        return FALSE;
+       }
+    }
+}
+
+
 /**
  * Get amount 
  *
